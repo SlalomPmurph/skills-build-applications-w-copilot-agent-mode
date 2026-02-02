c9e73bca583e3a8fe8a957bff57797f6abe86e06
SlalomPmurph <patrick.murphy@slalom.com>
2026-02-02 15:55:14 +0000

    Update ALLOWED_HOSTS and API root URLs for Codespace compatibility

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-w0fgoj6yvd#4*qv-mv5l!%n9@wg8c#4hgvu8xow!!0*qgr+t4!
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
